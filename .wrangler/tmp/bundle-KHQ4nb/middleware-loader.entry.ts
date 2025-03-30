c0f7e02f600ebf011f689ffa01a8665cb91313eb
Furanon <ahkukaku@gmail.com>
2025-03-30 15:33:36 +0700

    Update changes

--- .wrangler/tmp/bundle-KHQ4nb/middleware-loader.entry.ts
+++ .wrangler/tmp/bundle-KHQ4nb/middleware-loader.entry.ts
@@ -3,12 +3,12 @@
 // export dynamically through wrangler, or we can potentially let users directly
 // add them as a sort of "plugin" system.
 
-import ENTRY, { __INTERNAL_WRANGLER_MIDDLEWARE__ } from "/Users/ahkim/cloudflare-ticket-logger/.wrangler/tmp/bundle-xLzfkl/middleware-insertion-facade.js";
+import ENTRY, { __INTERNAL_WRANGLER_MIDDLEWARE__ } from "/Users/ahkim/cloudflare-ticket-logger/.wrangler/tmp/bundle-KHQ4nb/middleware-insertion-facade.js";
 import { __facade_invoke__, __facade_register__, Dispatcher } from "/Users/ahkim/.nvm/versions/node/v22.12.0/lib/node_modules/wrangler/templates/middleware/common.ts";
-import type { WorkerEntrypointConstructor } from "/Users/ahkim/cloudflare-ticket-logger/.wrangler/tmp/bundle-xLzfkl/middleware-insertion-facade.js";
+import type { WorkerEntrypointConstructor } from "/Users/ahkim/cloudflare-ticket-logger/.wrangler/tmp/bundle-KHQ4nb/middleware-insertion-facade.js";
 
 // Preserve all the exports from the worker
-export * from "/Users/ahkim/cloudflare-ticket-logger/.wrangler/tmp/bundle-xLzfkl/middleware-insertion-facade.js";
+export * from "/Users/ahkim/cloudflare-ticket-logger/.wrangler/tmp/bundle-KHQ4nb/middleware-insertion-facade.js";
 
 class __Facade_ScheduledController__ implements ScheduledController {
 	readonly #noRetry: ScheduledController["noRetry"];
